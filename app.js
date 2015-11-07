7e7ba278055b3a0ded0d7552fb2f34b8d8c7fadd
Nate Wiley <htmlnate@gmail.com>
2015-11-06 19:46:10 -0500

    minor

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express();
 // CORS middleware
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
