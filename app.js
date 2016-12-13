5a7d58ea41d0fb7e530941657c9439b897f09a21
Nate Wiley <htmlnate@gmail.com>
2016-12-13 17:28:02 -0500

    allow X-XSRF-TOKEN in req headers

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if(cluster.isMaster){
 	var allowCrossDomain = function(req, res, next) {
 			res.header('Access-Control-Allow-Origin', '*');
 			res.header('Access-Control-Allow-Methods', 'GET');
-			res.header('Access-Control-Allow-Headers', 'Content-Type');
+			res.header('Access-Control-Allow-Headers', 'Content-Type, X-XSRF-TOKEN');
 			next();
 	}
 
